a8831bc52595675870ebe6db6ae05cc4e655d72a
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-10-01 10:23:32 +0400

    Add files via upload

---
+++ springfirst/src/main/java/com/springcore/App.java
@@ -0,0 +1,15 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+    System.out.println("Hello World");
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/config.xml");
+        Student student1 = (Student) context.getBean("student1");
+        System.out.println(student1);
+
+    }
+}
