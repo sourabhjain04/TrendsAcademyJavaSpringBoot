8e6803bdfa95cfc1e94150ea9d121059263e8343
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-10-02 09:38:34 +0400

    Add files via upload

---
+++ SpringBootWithoutSecurity/src/main/java/com/example/springbootwithoutsecurity/SpringBootWithoutSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithoutsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithoutSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithoutSecurityApplication.class, args);
+    }
+
+}
---
+++ SpringBootWithoutSecurity/src/main/java/com/example/springbootwithoutsecurity/WelcomeRestController.java
@@ -0,0 +1,14 @@
+package com.example.springbootwithoutsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+
+    @GetMapping("/welcome")
+    public String getMessage() {
+        String msg="Hi this is With Security";
+        return msg;
+    }
+}
